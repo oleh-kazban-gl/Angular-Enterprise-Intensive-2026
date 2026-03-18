21d16597761c5e40d8a836218fb89c79ee47a6b9
Oleh Kazban <oleh.kazban@globallogic.com>
2026-03-18 18:13:59 +0200

    Create Post component

---
+++ libs/feature-post/src/lib/feature-post/post.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gl-post',
+  imports: [],
+  templateUrl: './post.component.html',
+  styleUrl: './post.component.scss',
+})
+export class PostComponent {}
