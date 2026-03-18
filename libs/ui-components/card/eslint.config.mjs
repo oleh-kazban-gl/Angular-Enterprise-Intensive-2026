3fff435d058b0bb971fefb7d1897693f1d30ba14
Oleh Kazban <oleh.kazban@globallogic.com>
2026-03-18 20:43:32 +0200

    Update Eslint configs

--- libs/ui-components/card/eslint.config.mjs
+++ libs/ui-components/card/eslint.config.mjs
@@ -1,4 +1,15 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../../eslint.config.mjs';
 
-export default [...baseConfig, ...nx.configs['flat/angular'], ...nx.configs['flat/angular-template']];
+export default [
+  ...baseConfig,
+  ...nx.configs['flat/angular'],
+  ...nx.configs['flat/angular-template'],
+  {
+    files: ['**/*.html'],
+    rules: {
+      '@angular-eslint/template/click-events-have-key-events': 'off',
+      '@angular-eslint/template/interactive-supports-focus': 'off',
+    },
+  },
+];
