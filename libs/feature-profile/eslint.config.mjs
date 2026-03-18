c417a189f3182ab3393c3afc5572024b957a3479
Oleh Kazban <oleh.kazban@globallogic.com>
2026-03-18 19:44:25 +0200

    Update configs

--- libs/feature-profile/eslint.config.mjs
+++ libs/feature-profile/eslint.config.mjs
@@ -1,8 +1,4 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
-  ...baseConfig,
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-];
+export default [...baseConfig, ...nx.configs['flat/angular'], ...nx.configs['flat/angular-template']];
