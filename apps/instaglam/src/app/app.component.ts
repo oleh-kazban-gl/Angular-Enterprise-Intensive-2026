d7f4bfb14db14c36a2e5eac91303892ae010200e
Oleh Kazban <oleh.kazban@globallogic.com>
2026-03-20 20:33:13 +0200

    Add new components

--- apps/instaglam/src/app/app.component.ts
+++ apps/instaglam/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { NavmenuComponent } from '@gl/feature-navmenu';
+import { ThemeService } from '@gl/util-services';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -10,4 +11,7 @@ import { NavmenuComponent } from '@gl/feature-navmenu';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  // Injecting ThemeService at root level ensures the effect runs from app startup
+  readonly themeService = inject(ThemeService);
+}
--- libs/feature-settings/src/lib/feature-settings/settings.component.ts
+++ libs/feature-settings/src/lib/feature-settings/settings.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { CardComponent } from '@gl/ui-components/card';
+import { ThemeService } from '@gl/util-services';
 
 @Component({
   selector: 'gl-settings',
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CardComponent, MatSlideToggleModule],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+  readonly themeService = inject(ThemeService);
+}
