631c83de054619bc6a368afae74f0abd580d62c2
Oleh Kazban <oleh.kazban@globallogic.com>
2026-03-21 13:33:17 +0200

    Update px with rem

--- libs/feature-auth/src/lib/sign-out/sign-out.component.ts
+++ libs/feature-auth/src/lib/sign-out/sign-out.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { LoadingComponent } from '@gl/ui-components/loading';
+
 @Component({
   selector: 'gl-sign-out',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: '',
+  imports: [LoadingComponent],
+  template: '<gl-loading />',
+  styles: [':host { display: flex; height: 100%; }'],
 })
 export class SignOutComponent {}
