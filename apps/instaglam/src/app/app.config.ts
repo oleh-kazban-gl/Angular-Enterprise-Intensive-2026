ff301ebb887f47c4604932d2c82a1c37313afe28
Oleh Kazban <oleh.kazban@globallogic.com>
2026-03-25 19:12:40 +0200

    Add HttpClient

--- apps/instaglam/src/app/app.config.ts
+++ apps/instaglam/src/app/app.config.ts
@@ -1,8 +1,9 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes), provideHttpClient()],
 };
